3c9ce2db8b5fc25559bdc9d375a0e1c0afac8f5c
RomanGenC <1romankadykov@gmail.com>
2024-03-25 18:57:36 +0300

    views and urls

---
+++ recipient/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from recipient import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('base/', views.base_page, name='base_page'),
+    path('self-cabinet/', views.self_cabinet, name='self_cabinet'),
+    path('all-recipients/', views.recipients_info, name='all_recipients'),
+    path('recipient-info/<int:recipient_id>/', views.recipient_info, name='recipient_info'),
+    path('user-info/<int:user_id>/', views.user_info, name='user_info'),
+    path('add-review/<int:recipient_id>/', views.add_review, name='add_review'),
+    path('success-review/', views.success_review, name='success_review'),
+    path('search/', views.recipient_search, name='recipient_search'),
+
+    path('politika-konfidencialnosti/', views.politika_konfidencialnosti, name='politika_konfidencialnosti'),
+    path('polzovatelskoe-soglashenie/', views.polzovatelskoe_soglashenie, name='polzovatelskoe_soglashenie'),
+    path('o-kompanii/', views.o_kompanii, name='o_kompanii'),
+
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+]
---
+++ recipient/views.py
@@ -0,0 +1,162 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib.auth.models import User
+from django.contrib.postgres.search import SearchVector
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.cache import cache
+from django.urls import reverse
+from django.db.models import Avg, Count
+from django.views.decorators.cache import cache_page
+
+from recipient.forms import LoginForm, UserRegistrationForm, AddReview, SearchForm
+from recipient.models import Review, Recipient
+
+
+def polzovatelskoe_soglashenie(request):
+    return render(request, 'about_site/polzovatelskoe_soglashenie.html')
+
+
+def politika_konfidencialnosti(request):
+    return render(request, 'about_site/politika_konfidencialnosti.html')
+
+
+@cache_page(60 * 15)
+def o_kompanii(request):
+    return render(request, 'about_site/o_kompanii.html')
+
+
+def base_page(request):
+    recipients = Recipient.objects.annotate(avg_rating=Avg('reviews__rate')).prefetch_related('specialities').order_by('-avg_rating')
+    users = User.objects.annotate(num_reviews=Count('reviews')).order_by('-num_reviews')
+
+    return render(request,
+                  'recipient/base_page.html',
+                  {'users': users, 'recipients': recipients})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Вы успешно аунтентифицировались!')
+                else:
+                    return HttpResponse('Аутентификация не прошла!')
+            else:
+                return HttpResponse('Невалидный аккаунт!')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,
+                          'registration/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'registration/register.html',
+                  {"user_form": user_form})
+
+
+@login_required
+def self_cabinet(request):
+    user = get_object_or_404(User, username=request.user.username)
+    reviews = Review.objects.select_related('recipient', 'user').prefetch_related('recipient__specialities').filter(user=user)
+
+    return render(request,
+                  'recipient/self_cabinet.html',
+                  {"user": user,
+                   "reviews": reviews})
+
+
+def recipients_info(request):
+    recipients = Recipient.objects.prefetch_related('specialities')
+
+    return render(request,
+                  'recipient/all_recipients.html',
+                  {'recipients': recipients
+                   })
+
+
+def recipient_info(request, recipient_id):
+    recipient = Recipient.objects.prefetch_related('specialities', 'reviews').get(pk=recipient_id)
+
+    return render(request,
+                  'recipient/recipient_info.html',
+                  {'recipient': recipient})
+
+
+def user_info(request, user_id):
+    user = User.objects.prefetch_related('reviews').order_by('-reviews').get(pk=user_id)
+
+    return render(request,
+                  'recipient/user_info.html',
+                  {'user': user})
+
+
+@login_required
+def add_review(request, recipient_id):
+    recipient = get_object_or_404(Recipient.objects.prefetch_related("specialities"), pk=recipient_id)
+
+    if request.method == 'POST':
+        form = AddReview(request.POST)
+
+        if form.is_valid():
+            review_object = form.save(commit=False)
+            review_object.recipient = recipient
+            if request.user.is_authenticated:
+                review_object.user = request.user
+
+            if request.META.get('REMOTE_ADDR'):
+                review_object.user_ip = request.META.get('REMOTE_ADDR')
+
+            review_object.save()
+            return redirect(reverse('success_review'))
+        else:
+            messages.error(request, 'Должно быть хотя бы 100 символов')
+
+    else:
+        form = AddReview()
+
+    return render(request,
+                  'recipient/add_review.html',
+                  {'recipient': recipient, 'form': form
+                   })
+
+
+def success_review(request):
+    return render(request, 'recipient/success_review.html', )
+
+
+def recipient_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Recipient.objects.filter(first_name__icontains=query)
+
+    return render(request,
+                  'recipient/search_recipient.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
