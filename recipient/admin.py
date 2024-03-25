ae4d0eafe1064b0135fdc0c352a9635814bcd5d9
RomanGenC <1romankadykov@gmail.com>
2024-03-25 18:59:14 +0300

    forms and admin

---
+++ recipient/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import Review, Recipient, Specialty
+from django.db import models
+from django import forms
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'recipient', 'original_review', 'modified_review', 'time_create', 'time_update', 'is_published', 'user',
+        'user_ip'
+    )
+    list_filter = ('time_create',)
+    search_fields = ['doctor', 'original_review', 'modified_review']
+    readonly_fields = ('time_create', 'original_review')
+    autocomplete_fields = ('recipient',)
+    list_per_page = 10
+    formfield_overrides = {
+        models.CharField: {'widget': forms.Textarea(attrs={'rows': 10, 'cols': 40})},
+    }
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('recipient', 'user')
+
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'second_name', 'last_name', 'description', 'experience', 'photo')
+    list_filter = ('second_name',)
+    search_fields = ('first_name', 'second_name', 'last_name')
+    list_per_page = 10
+
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('title',)
+    search_fields = ('title',)
+    list_per_page = 10
---
+++ recipient/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Recipient, Review
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Введите пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cd['password2']
+
+
+class AddReview(forms.ModelForm):
+    is_anonymous = forms.BooleanField(label='Анонимный отзыв', required=False)
+
+    class Meta:
+        model = Review
+        fields = ['original_review', 'rate']
+
+        widgets = {
+            'original_review': forms.Textarea(attrs={'cols': 50, 'rows': 5,
+                                                     'placeholder': 'Напишите ваш отзыв здесь...'}),
+            'rate': forms.Select(choices=[(i, str(i)) for i in range(1, 6)]),
+        }
+
+    def clean_original_review(self):
+        original_review = self.cleaned_data['original_review']
+        if len(original_review) < 100:
+            raise ValidationError("Должно быть хотя бы 100 символов")
+        return original_review
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
